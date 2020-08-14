1da061a648baa284746818fb3bf64d6d5bfab4f8
ghilbut <ghilbut@gmail.com>
2020-08-15 02:50:03 +0900

    Complete run landgin page and mts page on exchange project

--- modules/byfs-common/aws-lb-public.tf
+++ modules/byfs-common/aws-lb-public.tf
@@ -53,52 +53,13 @@ resource aws_acm_certificate_validation alb_public {
 }
 
 ##--------------------------------------------------------------
-##  aws security group for public
-
-resource aws_security_group alb_public {
-  name        = "${var.unique_name}-alb-public"
-  description = "Allow all traffic"
-  vpc_id      = data.aws_vpc.default.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = -1
-    cidr_blocks     = ["0.0.0.0/0"]
-  }
-
-  tags = merge(
-    map(
-      "Name", "sg-${var.unique_name}-alb-public",
-    ),
-    local.tags,
-  )
-}
-
-##--------------------------------------------------------------
 ##  aws application load balancer
 
 resource aws_lb alb_public {
   name               = "alb-${var.unique_name}-public"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_public.id]
+  security_groups    = [aws_security_group.public.id]
   subnets            = data.aws_subnet.defaults.*.id
 
   tags = merge(
